af7c0b23e58508c5a2d30e3b552db8b096a6922f
mepha89 <umamarahman12@gmail.com>
2023-11-17 19:33:40 -0330

    UR-update-input-for-patient-model

--- lib/models/patient_model.dart
+++ lib/models/patient_model.dart
@@ -5,6 +5,13 @@ class PatientModel {
   final String name;
   final String phone;
   final String email;
+  final int age;
+  final int weight;
+  final int height;
+  final String street;
+  final String city;
+  final String province;
+  final String postalCode;
   final List<String>? appointments;
 
   PatientModel({
@@ -12,6 +19,13 @@ class PatientModel {
     required this.name,
     required this.phone,
     required this.email,
+    required this.age,
+    required this.weight,
+    required this.height,
+    required this.street,
+    required this.city,
+    required this.province,
+    required this.postalCode,
     this.appointments,
   });
 
@@ -21,6 +35,13 @@ class PatientModel {
       'name': name,
       'phone': phone,
       'email': email,
+      'age': age,
+      'weight': weight,
+      'height': height,
+      'street': street,
+      'city': city,
+      'province': province,
+      'postalCode': postalCode,
       'appointments': appointments,
     };
   }
@@ -32,6 +53,13 @@ class PatientModel {
       name: data['name'],
       phone: data['phone'],
       email: data['email'],
+      age: data['age'],
+      weight: data['weight'],
+      height: data['height'],
+      street: data['street'],
+      city: data['city'],
+      province: data['province'],
+      postalCode: data['postalCode'],
       appointments: data['appointments'],
     );
   }
