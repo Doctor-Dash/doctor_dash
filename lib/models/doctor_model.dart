2cb5d648074cbaaeeeb422105dc1d2fb0c724157
ShairYousuf <syjahin@mun.ca>
2023-11-17 16:56:39 -0330

    Created the implementation of the Doctor model

--- lib/models/doctor_model.dart
+++ lib/models/doctor_model.dart
@@ -1,5 +1,58 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DockerModel{
+class DocterModel {
+  final String doctorID;
+  final String name;
+  final String phone;
+  final String email;
+  final String speciality;
+  final String clinicID;
+  final List<String> availability;
+  final List<String> rating;
+  final List<String> appointmentID;
+  final List<String> feedbackID;
 
+  DocterModel({
+    required this.doctorID,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.speciality,
+    required this.clinicID,
+    required this.availability,
+    required this.rating,
+    required this.appointmentID,
+    required this.feedbackID,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'doctorID': doctorID,
+      'name': name,
+      'phone': phone,
+      'email': email,
+      'speciality': speciality,
+      'clinicID': clinicID,
+      'availability': availability,
+      'rating': rating,
+      'appointmentID': appointmentID,
+      'feedbackID': feedbackID,
+    };
+  }
+
+  static DocterModel fromMap(DocumentSnapshot doc) {
+    Map<String, dynamic> map = doc.data() as Map<String, dynamic>;
+    return DocterModel(
+      doctorID: map['doctorID'],
+      name: map['name'],
+      phone: map['phone'],
+      email: map['email'],
+      speciality: map['speciality'],
+      clinicID: map['clinicID'],
+      availability: List<String>.from(map['availability']),
+      rating: List<String>.from(map['rating']),
+      appointmentID: List<String>.from(map['appointmentID']),
+      feedbackID: List<String>.from(map['feedbackID']),
+    );
+  }
 }
