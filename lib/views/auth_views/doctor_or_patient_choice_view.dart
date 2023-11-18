7918964eb289509102d0614c561614b8da054151
mepha89 <umamarahman12@gmail.com>
2023-11-17 23:30:26 -0330

    UR-doctorOrPatient-choice-screen

---
+++ lib/views/auth_views/doctor_or_patient_choice_view.dart
@@ -0,0 +1,48 @@
+import 'package:doctor_dash/views/patient_views.dart/patient_signin.dart';
+import 'package:flutter/material.dart';
+
+class DoctorOrPatientChoice extends StatelessWidget {
+  const DoctorOrPatientChoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Doctor-Patient Sign Up/Login'),
+      ),
+      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Choose your role:',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => DoctorSignInPage()),
+                // );
+              },
+              child: const Text('Doctor'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PatientSignIn()),
+                );
+              },
+              child: const Text('Patient'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
