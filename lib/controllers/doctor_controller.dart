90a89803642e921d204d7f7318f48154e683c199
Daayim Asim <daayimasim81@gmail.com>
2023-11-17 22:55:45 -0330

    Using clinic_id for getDoctors()

--- lib/controllers/doctor_controller.dart
+++ lib/controllers/doctor_controller.dart
@@ -1,16 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../models/doctor_model.dart'; // Correct the path as necessary
+import '../models/doctor_model.dart';
+import './clinic_controller.dart';
 
 class DoctorService {
   final User? user = FirebaseAuth.instance.currentUser;
   final CollectionReference doctorsCollection;
+  final ClinicService clinicService;
 
   DoctorService()
       : doctorsCollection = FirebaseFirestore.instance
             .collection('users')
             .doc(FirebaseAuth.instance.currentUser?.uid)
-            .collection('Doctors');
+            .collection('Doctors'),
+        clinicService = ClinicService();
 
   Future<DocumentReference<Object?>> addDoctor(DoctorModel doctor) async {
     if (user == null) {
@@ -58,8 +61,8 @@ class DoctorService {
   }
 
   // Get list of doctors by search parameters
-  Stream<List<DoctorModel>> getDoctors(
-      String speciality, String searchCity) async* {
+  Future<List<DoctorModel>> getDoctors(
+      String speciality, String searchCity) async {
     if (user == null) {
       throw FirebaseAuthException(
         code: 'unauthenticated',
@@ -68,12 +71,8 @@ class DoctorService {
     }
 
     try {
-      // Step 1: Fetch clinics in the specified city
-      var clinicsSnapshot = await FirebaseFirestore.instance
-          .collection('clinics')
-          .where('city', isEqualTo: searchCity)
-          .get();
-
+      // Step 1: Fetch clinics in the specified city using ClinicService
+      var clinicsSnapshot = await clinicService.getClinicsInCity(searchCity);
       var clinicIds = clinicsSnapshot.docs.map((doc) => doc.id).toList();
 
       // Step 2: Fetch doctors with the specified specialty and clinic IDs
@@ -82,11 +81,9 @@ class DoctorService {
           .where('clinicID', arrayContainsAny: clinicIds);
 
       var doctorsSnapshot = await doctorsQuery.get();
-
-      var doctors =
-          doctorsSnapshot.docs.map((doc) => DoctorModel.fromMap(doc)).toList();
-
-      yield doctors;
+      return doctorsSnapshot.docs
+          .map((doc) => DoctorModel.fromMap(doc))
+          .toList();
     } catch (e) {
       throw Exception('Error fetching doctors list: $e');
     }
