1c61f65ebbadd53b53bff7c98631f0061f9f9e9b
mepha89 <umamarahman12@gmail.com>
2023-11-17 23:30:12 -0330

    UR-patient-signin

---
+++ lib/views/patient_views.dart/patient_signin.dart
@@ -0,0 +1,26 @@
+import 'package:doctor_dash/main.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+class PatientSignIn extends StatelessWidget {
+  const PatientSignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(), // new
+            ],
+          );
+        }
+
+        return const MyHomePage(title: 'Flutter Demo Home Page');
+      },
+    );
+  }
+}
