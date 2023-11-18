1ca6c9d2b8c2de92990e6aad4990e99eca961526
Shair Yousuf Jahin <71799869+ShairYousuf@users.noreply.github.com>
2023-11-17 22:03:52 -0330

    23 set up patient controller (#33)
    * put all the models in the models folder
    
    * Added addPatient function
    
    * Add getPatient method to PatientService
    
    * Add getPatientAppointments method to
    PatientService
    
    * Add error handling and logging to patient
    controller methods

---
+++ lib/controllers/patient_controller.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/patient_model.dart';
+
+class PatientService {
+  final user = FirebaseAuth.instance.currentUser;
+  final CollectionReference patientCollection;
+
+  PatientService()
+      : patientCollection = FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .collection('patients');
+
+  Future<DocumentReference<Object?>> addPatient(PatientModel patient) async {
+    try {
+      return await patientCollection.add(patient.toMap());
+    } catch (e) {
+      print('Failed to add patient: $e');
+      rethrow;
+    }
+  }
+
+  Future<QuerySnapshot> getPatient(String patientId) async {
+    try {
+      return await patientCollection
+          .where('patientId', isEqualTo: patientId)
+          .get();
+    } catch (e) {
+      print('Failed to get patient: $e');
+      rethrow;
+    }
+  }
+
+  Future<QuerySnapshot> getPatientAppointments(String patientId) async {
+    try {
+      var appointmentIds = await patientCollection
+          .doc(patientId)
+          .collection('appointments')
+          .get();
+      return appointmentIds;
+    } catch (e) {
+      print('Failed to get patient appointments: $e');
+      rethrow;
+    }
+  }
+
+  Future<void> updatePatient(PatientModel patient) async {
+    try {
+      await patientCollection.doc(patient.patientId).update(patient.toMap());
+    } catch (e) {
+      print('Failed to update patient: $e');
+      rethrow;
+    }
+  }
+}
