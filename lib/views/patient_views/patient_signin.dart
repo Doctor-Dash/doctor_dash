539b2c315ada61d2e21574cc90bd27c791fa35e5
mepha89 <umamarahman12@gmail.com>
2023-11-25 18:36:56 -0330

    UR-patient-signin-and-signup

--- lib/views/patient_views/patient_signin.dart
+++ lib/views/patient_views/patient_signin.dart
@@ -1,37 +1,109 @@
 import 'package:doctor_dash/main.dart';
-import 'package:doctor_dash/views/patient_views/patient_sign_up.dart';
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:doctor_dash/views/auth_views/password_reset_view.dart';
+import 'package:doctor_dash/views/patient_views/patient_signup.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+// import 'password_reset_view.dart';
 
-class PatientSignIn extends StatelessWidget {
+class PatientSignIn extends StatefulWidget {
   const PatientSignIn({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return SignInScreen(
-            providers: [
-              EmailAuthProvider(),
-            ],
-            subtitleBuilder: (context, action) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: action == AuthAction.signIn
-                    ? const Text(
-                        'Welcome to Doctor Dash, please sign in as a Patient!')
-                    : const Text(
-                        'Welcome to Doctor Dash, please sign up as a Patient!'),
-              );
-            },
-          );
-        }
+  _PatientSignInState createState() => _PatientSignInState();
+}
 
-        return PatientSignUp();
-      },
+class _PatientSignInState extends State<PatientSignIn> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  String _errorMessage = '';
+
+  Future<void> _signIn() async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      // Navigate to home if successful
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+            builder: (_) => const MyHomePage(
+                  title: "Patient's Search Page",
+                )),
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        final regex = RegExp(r'\[([^)]+)\]');
+        final match = regex.firstMatch(e.message ?? '');
+        String extractedError = match?.group(1) ?? 'Invalid email';
+
+        // Making the first letter uppercase and the rest lowercase,
+        // and replacing underscores with spaces
+        _errorMessage = extractedError.toLowerCase().replaceAll('_', ' ');
+        _errorMessage =
+            _errorMessage[0].toUpperCase() + _errorMessage.substring(1);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign In')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: _signIn,
+              child: const Text('Sign In'),
+            ),
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(_errorMessage,
+                    style: const TextStyle(color: Colors.black)),
+              ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => PatientSignUp()),
+                );
+              },
+              child: const Text('Don’t have an account? Sign up'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => PasswordResetView()),
+                );
+              },
+              child: const Text('Forgot Password? Reset Password'),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
 }
---
+++ lib/views/patient_views/patient_signup.dart
@@ -0,0 +1,88 @@
+// lib/view/signup_view.dart
+
+import 'package:doctor_dash/views/patient_views/create_patient.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'patient_signin.dart'; // Import your sign-in view
+
+class PatientSignUp extends StatefulWidget {
+  const PatientSignUp({super.key});
+
+  @override
+  _PatientSignUpState createState() => _PatientSignUpState();
+}
+
+class _PatientSignUpState extends State<PatientSignUp> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  String _errorMessage = '';
+
+  Future<void> _signUp() async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      // Navigate to sign-in on successful sign-up
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => const CreatePatient()),
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = e.message ?? 'An error occurred';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sign Up')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: _signUp,
+              child: Text('Sign Up'),
+            ),
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: Text(_errorMessage, style: TextStyle(color: Colors.red)),
+              ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => PatientSignIn()),
+                );
+              },
+              child: const Text('Already have an account? Sign in'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
