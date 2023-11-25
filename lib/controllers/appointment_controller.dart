4f919ab7e624559c27ceb700592cc6dbc1b23d16
daayim <daayimasim81@gmail.com>
2023-11-25 19:20:41 -0330

    Improved apointment controller removed unnecessary < object?> type

--- lib/controllers/appointment_controller.dart
+++ lib/controllers/appointment_controller.dart
@@ -1,55 +1,80 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../models/appointment_model.dart';
 
 class AppointmentService {
-  final CollectionReference appointmentCollection =
-      FirebaseFirestore.instance.collection('appointments');
+  final User? user = FirebaseAuth.instance.currentUser;
+  final CollectionReference appointmentCollection;
 
-  Future<void> addAppointment(AppointmentModel appointment) async {
+  AppointmentService()
+      : appointmentCollection =
+            FirebaseFirestore.instance.collection('appointments');
+
+  Future<DocumentReference> addAppointment(AppointmentModel appointment) async {
+    _ensureAuthenticated();
+
     try {
-      await appointmentCollection.add(appointment.toMap());
-    } catch (error) {
-      throw Exception('Error adding appointment: $error');
+      return await appointmentCollection.add(appointment.toMap());
+    } on FirebaseAuthException catch (authError) {
+      throw Exception('Authentication Error: ${authError.message}');
+    } on FirebaseException catch (firestoreError) {
+      throw Exception('Firestore Error: ${firestoreError.message}');
     }
   }
 
   Future<QuerySnapshot> getAppointmentsForPatient(String patientId) async {
+    _ensureAuthenticated();
+
     try {
       return await appointmentCollection
           .where('patientId', isEqualTo: patientId)
           .get();
-    } catch (error) {
-      throw Exception('Error fetching appointments: $error');
+    } catch (e) {
+      throw Exception('Error fetching appointments: $e');
     }
   }
 
-  Future<QuerySnapshot> getAppointmentsForDoctor(String doctoerId) async {
+  Future<QuerySnapshot> getAppointmentsForDoctor(String doctorId) async {
+    _ensureAuthenticated();
+
     try {
       return await appointmentCollection
-          .where('doctoerId', isEqualTo: doctoerId)
+          .where('doctorId', isEqualTo: doctorId)
           .get();
-    } catch (error) {
-      throw Exception('Error fetching appointments: $error');
+    } catch (e) {
+      throw Exception('Error fetching appointments: $e');
     }
   }
 
   Future<void> updateAppointment(AppointmentModel updatedAppointment) async {
+    _ensureAuthenticated();
+
     try {
-      // Assuming updatedAppointment has a valid appointmentId
       final appointmentDoc =
           appointmentCollection.doc(updatedAppointment.appointmentId);
       await appointmentDoc.update(updatedAppointment.toMap());
-    } catch (error) {
-      throw Exception('Error updating appointment: $error');
+    } catch (e) {
+      throw Exception('Error updating appointment: $e');
     }
   }
 
   Future<void> deleteAppointment(String appointmentId) async {
+    _ensureAuthenticated();
+
     try {
       final appointmentDoc = appointmentCollection.doc(appointmentId);
       await appointmentDoc.delete();
-    } catch (error) {
-      throw Exception('Error deleting appointment: $error');
+    } catch (e) {
+      throw Exception('Error deleting appointment: $e');
+    }
+  }
+
+  void _ensureAuthenticated() {
+    if (user == null) {
+      throw FirebaseAuthException(
+        code: 'unauthenticated',
+        message: 'User must be logged in to perform this operation.',
+      );
     }
   }
 
