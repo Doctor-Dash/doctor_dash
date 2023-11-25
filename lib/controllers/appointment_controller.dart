dcbbe6f21ea6b37ca6a8c825024ae593c7dde98a
daayim <daayimasim81@gmail.com>
2023-11-25 17:45:30 -0330

    Fixed class name

--- lib/controllers/appointment_controller.dart
+++ lib/controllers/appointment_controller.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/appointment_model.dart';
 
-class AppointmentController {
+class AppointmentService {
   final CollectionReference appointmentCollection =
       FirebaseFirestore.instance.collection('appointments');
 
