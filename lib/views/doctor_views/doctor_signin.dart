546fb5154e8314df346bc0dbdbdc4840d348b777
Osama Khadim <92998267+crimsix-7@users.noreply.github.com>
2023-11-25 23:49:15 -0330

    Update doctor_signin.dart

--- lib/views/doctor_views/doctor_signin.dart
+++ lib/views/doctor_views/doctor_signin.dart
@@ -2,7 +2,7 @@ import 'package:doctor_dash/main.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'doctor_signup.dart'; // Adjust the import path as necessary
+import 'doctor_signup.dart'; 
 
 class DoctorSignIn extends StatelessWidget {
   const DoctorSignIn({super.key});
@@ -13,7 +13,6 @@ class DoctorSignIn extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          // User is not signed in, show the sign-in screen
           return SignInScreen(
             providers: [
               EmailAuthProvider(),
@@ -28,8 +27,7 @@ class DoctorSignIn extends StatelessWidget {
             },
           );
         } else {
-          // User is signed in, show the main app screen or dashboard
-          return DoctorSignUpPage();  // You need to define this widget according to your app's structure
+          return DoctorSignUpPage(); 
         }
 
         return const MyHomePage(
